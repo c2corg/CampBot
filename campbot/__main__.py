cd01d1e9423995bfaf3c6e404678d2a49aa901dc
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2017-10-29 19:35:44 +0100

    main

--- campbot/__main__.py
+++ campbot/__main__.py
@@ -3,6 +3,7 @@ CampBot, Python bot framework for camptocamp.org
 
 Usage:
   campbot check_voters <message_url> --login=<login> --password=<password>
+  campbot fix_markdown <ids_file> --login=<login> --password=<password>
 
 Options:
   --login=<login>          Bot login
@@ -14,10 +15,22 @@ from docopt import docopt
 
 args = docopt(__doc__)
 
-if args["check_voters"]:
+
+def get_campbot():
     from campbot import CampBot
 
     bot = CampBot()
     bot.login(login=args["--login"], password=args["--password"])
 
-    bot.check_voters(url=args["<message_url>"])
+    return bot
+
+
+if args["check_voters"]:
+    get_campbot().check_voters(url=args["<message_url>"])
+
+elif args["fix_markdown"]:
+    from campbot.utils import get_ids_from_file
+    from campbot.processors import BBCodeRemover
+
+    ids = get_ids_from_file(args["<ids_file>"])
+    get_campbot().fix_markdown(BBCodeRemover(), **ids)
---
+++ campbot/utils.py
@@ -0,0 +1,16 @@
+def get_ids_from_file(filename):
+    result = {}
+    with open(filename) as f:
+        for line in f:
+            line = line.replace(" ", "").replace("\n", "")
+            item_id, item_type = line.split("|")
+
+            item_type = {"u": "user_ids",
+                         "a": "area_ids",
+                         "r": "route_ids"}[item_type]
+
+            if item_type not in result:
+                result[item_type] = []
+            result[item_type].append(int(item_id))
+
+    return result
