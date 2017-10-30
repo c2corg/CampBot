8df3bfb85dc15e4af56042bda3d86cc1c337eb7a
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2017-10-31 00:26:31 +0100

    version

--- campbot/__init__.py
+++ campbot/__init__.py
@@ -5,4 +5,4 @@ campbot is a package for automatic edition of camptocamp.org
 from campbot.core import CampBot
 from campbot.processors import BBCodeRemover
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
