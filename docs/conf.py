c98d2a4918ee1a4d5ef88f0e7acf9170633c5361
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2018-06-22 15:13:36 +0000

    conf

--- docs/conf.py
+++ docs/conf.py
@@ -1,2 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, unicode_literals
+
+from datetime import datetime
+
+from recommonmark.parser import CommonMarkParser
 
 extensions = ["sphinx.ext.autodoc",]
+templates_path = ['/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx', 'templates', '_templates', '.templates']
+source_suffix = ['.rst', '.md']
+source_parsers = {
+            '.md': CommonMarkParser,
+        }
+master_doc = 'index'
+project = u'CampBot'
+copyright = "Charles de Beauchesne " + str(datetime.now().year)
+version = 'latest'
+release = 'latest'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+htmlhelp_basename = 'campbot'
+html_theme = 'sphinx_rtd_theme'
+file_insertion_enabled = False
+latex_documents = [
+  ('index', 'campbot.tex', u'CampBot Documentation',
+   u'', 'manual'),
+]
