6bd0550ec604b4026d313fc5059a173f4285a1af
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2018-07-19 11:48:23 +0000

    Shorter code

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -9,67 +9,80 @@ def get_message(filename):
     return json.load(io.open(filename, encoding="utf8"))
 
 
+def _wiki(method, url, answer):
+    return method, r"https://api.camptocamp.org/" + url, answer,
+
+
+def _forum(method, url, answer):
+    return method, r"https://forum.camptocamp.org/" + url, answer,
+
+
 messages = [
     # auth
-    ("GET", r"https://forum.camptocamp.org/session/csrf", {"csrf": "csrf"}),
-    ("POST", r"https://api.camptocamp.org/users/login", {"token": "", "redirect_internal": "/sso", "roles": []}),
-    ('GET', r'https://forum.camptocamp.org/sso', ""),
+    _forum("GET", r"session/csrf", {"csrf": "csrf"}),
+    _forum('GET', r'sso', ""),
+    _wiki("POST", r"users/login", {"token": "", "redirect_internal": "/sso", "roles": []}),
 
     # specific responses
-    ('GET', r'https://api.camptocamp.org/(outings|routes|articles|waypoints)\?.*offset=30.*', {"documents": []}),
-    ('GET', r'https://api.camptocamp.org/routes/123/fr/(123|122)', get_message("redirection")),
-    ('GET', r'https://api.camptocamp.org/articles/996571', get_message("conf_replacements")),
-    ('GET', r'https://api.camptocamp.org/routes/293549/fr/(1738922|1738528)', get_message("route_version")),
-    ('GET', 'https://api.camptocamp.org/routes/293549/fr/(880880|978249|478470|1738923)', get_message("route_version2")),
-    ('GET', 'https://api.camptocamp.org/routes/952126', {'protected': True, 'document_id': 952126}),
-    ('GET', 'https://api.camptocamp.org/routes/952167', {'redirects_to': 952126}),
-    ('GET', r'https://api.camptocamp.org/profiles/3199', {'document_id': 3199}),
-    ('GET', r'https://api.camptocamp.org/documents/changes\?.*&u=3199.*', {'feed': []}),
-    ('GET', r'https://api.camptocamp.org/profiles/666', Exception()),
-    ('GET', r'https://api.camptocamp.org/profiles/667',
-     {'document_id': 666, 'forum_username': "new_user", "name": "new_user", "categories": []}),
-    ('GET', r'https://api.camptocamp.org/profiles/668',
-     {'document_id': 666, 'forum_username': "new_user", "name": "new_user", "categories": ['club']}),
+    _wiki('GET', r'(outings|routes|articles|waypoints)\?.*offset=30.*', {"documents": []}),
+    _wiki('GET', r'routes/123/fr/(123|122)', get_message("redirection")),
+    _wiki('GET', r'articles/996571', get_message("conf_replacements")),
+    _wiki('GET', r'routes/293549/fr/(1738922|1738528)', get_message("route_version")),
+    _wiki('GET', r'routes/293549/fr/(880880|978249|478470|1738923)', get_message("route_version2")),
+    _wiki('GET', r'routes/952126', {'protected': True, 'document_id': 952126}),
+    _wiki('GET', r'routes/952167', {'redirects_to': 952126}),
+    _wiki('GET', r'profiles/3199', {'document_id': 3199}),
+    _wiki('GET', r'documents/changes\?.*&u=3199.*', {'feed': []}),
+    _wiki('GET', r'profiles/666', Exception()),
+
+    _wiki('GET', r'profiles/667', {'document_id': 666,
+                                   'forum_username': "new_user",
+                                   "name": "new_user",
+                                   "categories": []}),
+
+    _wiki('GET', r'profiles/668', {'document_id': 666,
+                                   'forum_username': "new_user",
+                                   "name": "new_user",
+                                   "categories": ['club']}),
 
     # documents
-    ('GET', r'https://api.camptocamp.org/outings/\d+', get_message("outing")),
-    ('GET', r'https://api.camptocamp.org/profiles/\d+', get_message("user")),
-    ('GET', r'https://api.camptocamp.org/routes/\d+', get_message("route")),
-    ('GET', r'https://api.camptocamp.org/waypoints/\d+', get_message("waypoint")),
-    ('GET', r'https://api.camptocamp.org/images/\d+', get_message("image")),
-    ('GET', r'https://api.camptocamp.org/articles/\d+', get_message("article")),
-    ('GET', r'https://api.camptocamp.org/areas/\d+', get_message("area")),
-    ('GET', r'https://api.camptocamp.org/books/\d+', {}),
-    ('GET', r'https://api.camptocamp.org/xreports/\d+', {}),
-    ('GET', r'https://api.camptocamp.org/maps/\d+', {}),
+    _wiki('GET', r'outings/\d+', get_message("outing")),
+    _wiki('GET', r'profiles/\d+', get_message("user")),
+    _wiki('GET', r'routes/\d+', get_message("route")),
+    _wiki('GET', r'waypoints/\d+', get_message("waypoint")),
+    _wiki('GET', r'images/\d+', get_message("image")),
+    _wiki('GET', r'articles/\d+', get_message("article")),
+    _wiki('GET', r'areas/\d+', get_message("area")),
+    _wiki('GET', r'books/\d+', {}),
+    _wiki('GET', r'xreports/\d+', {}),
+    _wiki('GET', r'maps/\d+', {}),
 
     # document list
-    ('GET', r'https://api.camptocamp.org/outings(\?.*)?', get_message("outings")),
-    ('GET', r'https://api.camptocamp.org/routes(\?.*)?', get_message("routes")),
-    ('GET', r'https://api.camptocamp.org/xreports(\?.*)?', get_message("routes")),
-    ('GET', r'https://api.camptocamp.org/waypoints(\?.*)?', get_message("routes")),
+    _wiki('GET', r'outings(\?.*)?', get_message("outings")),
+    _wiki('GET', r'routes(\?.*)?', get_message("routes")),
+    _wiki('GET', r'xreports(\?.*)?', get_message("routes")),
+    _wiki('GET', r'waypoints(\?.*)?', get_message("routes")),
 
     # recent changes
-    ("GET", r"https://api.camptocamp.org/documents/changes\?limit=50&token=1687339", {"feed": []}),
-    ("GET", r"https://api.camptocamp.org/documents/changes.*", get_message("changes")),
+    _wiki("GET", r"documents/changes\?limit=50&token=1687339", {"feed": []}),
+    _wiki("GET", r"documents/changes.*", get_message("changes")),
 
     # forum
-    ('GET', r'https://forum.camptocamp.org/t/\d+.json\?username_filters=rabot', {"last_posted_at": "2017-12-21"}),
-    ('GET', r'https://forum.camptocamp.org/t/\d+.json', get_message("topic")),
-    ('GET', r'https://forum.camptocamp.org/posts/\d+.json', get_message("post")),
-    ('GET', r'https://forum.camptocamp.org/groups/\w+/members.json.*', get_message("groups")),
-    ('GET', r'https://forum.camptocamp.org/groups/Association.json', get_message("groups_Association")),
+    _forum('GET', r't/\d+.json\?username_filters=rabot', {"last_posted_at": "2017-12-21"}),
+    _forum('GET', r't/\d+.json', get_message("topic")),
+    _forum('GET', r'posts/\d+.json', get_message("post")),
+    _forum('GET', r'groups/\w+/members.json.*', get_message("groups")),
+    _forum('GET', r'groups/Association.json', get_message("groups_Association")),
 
     # Misc
-    ('GET', r'https://api.camptocamp.org/search.*', get_message("search_user")),
-    ('POST', 'https://forum.camptocamp.org/posts', {}),
+    _wiki('GET', r'search.*', get_message("search_user")),
+    _forum('POST', r'posts', {}),
+    _forum('GET', r'polls/voters.json.*&offset=0', {"poll": {"option_id": [{"username": "CharlesB"},
+                                                                           {"username": "grimpeur8b"},
+                                                                           {"username": "charlesdet"}]}}),
+    _forum('GET', r'polls/voters.json.*&offset=1', {"poll": {}}),
+
     ('PUT', '.*', {}),
-
-    ('GET', r'https://forum.camptocamp.org/polls/voters.json.*&offset=0',
-     {"poll": {"option_id": [{"username": "CharlesB"}, {"username": "grimpeur8b"}, {"username": "charlesdet"}]}}),
-
-    ('GET', r'https://forum.camptocamp.org/polls/voters.json.*&offset=1',
-     {"poll": {}}),
 ]
 
 
